01616ccd0b0192476b0479519148f4cfa3032174
nhjeon <nakhyun.jeon@gmail.com>
2020-08-11 14:40:26 +0900

    feat: add logging format

--- {{cookiecutter.project_slug}}/app/main.py
+++ {{cookiecutter.project_slug}}/app/main.py
@@ -1,6 +1,11 @@
+import logging
+
 from fastapi import FastAPI
 
 from api import api_router
 
 app = FastAPI()
 app.include_router(api_router)
+
+FORMAT = '[%(asctime)s] [%(threadName)s(%(thread)d) [%(filename)s-%(lineno)d] [%(levelname)s] %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
